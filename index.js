e45ce602567211863a47b944f6b1f9b8ce99c87b
Lotanna1989 <78662437+Lotanna1989@users.noreply.github.com>
2021-12-31 09:21:05 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let data = ['Superman', 'Batman', 'Hulk','Blackwindow', 'Robin','Aquaman','Ironman','Wonderwoman','Shazam','Cyborg'];
+
+for (let i=0; i<=4; i++)
+{
+ console.log(data[i]);
+}
+console.log('The 5th character/iteration is Robin');
+console.log(data.length);
+
